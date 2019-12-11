88fdc745434a3f6baf99939fe643de5bc676264b
Peter <petr.chernykh.97@gmail.com>
2019-12-11 19:51:44 +0200

    Dec/11/2019

---
+++ ALevel_Practise7/ALevel_Practise7/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALevel_Practise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = false;
+            int menuChoice;
+
+            while (!check)
+            {
+                Console.WriteLine("Please choose the required option \n 1.Add new sportsman \n 2. Show process \n 3. Exit");
+                while (!int.TryParse(Console.ReadLine(), out menuChoice) || menuChoice < 1 || menuChoice > 3)
+                    Console.WriteLine("Incorrect number. Please use numbers from 1 to 3");
+
+                switch (menuChoice)
+                {
+                    case 1:
+                    var addPerson =AddNewVisitors();
+                    VisitorsKeeper.Add(addPerson);
+                        
+
+                        break;
+                    case 2: 
+                       ShowProcess();
+                        break;
+                    case 3:
+                        return;
+                }
+            }
+
+        }
+        static Sportsman AddNewVisitors()
+        {
+            Console.WriteLine("Please enter the name of the person");
+            var personName = Console.ReadLine();
+            Console.WriteLine("Please enter the amount of the exersices");
+            var amountOfExercises = int.Parse(Console.ReadLine());
+
+            var sportsmanKeeper = new Sportsman(personName, amountOfExercises);
+
+            return sportsmanKeeper;
+        }
+
+        static void ShowProcess()
+        {
+            if (VisitorsKeeper.Visitor.Any())
+            {
+                foreach (var sportsman in VisitorsKeeper.Visitor)
+                {
+                    sportsman.exercisesLeft--;
+
+                }
+                VisitorsKeeper.Visitor.RemoveAll(sportsman => sportsman.exercisesLeft.Equals(-1));//RemoveAll удаляет все, в скобках мы задаем условия при которых эти "все" будут удалены Equals(-1)
+                //(значит, что когда значение exercisesLeft будет -1, то этот элемент будет удален с листа)
+                foreach (var sportsman in VisitorsKeeper.Visitor)
+                {
+                    Console.WriteLine(sportsman.ToString());
+
+                }
+            }
+        }
+
+    }
+}
---
+++ ALevel_Practise7/ALevel_Practise7/Sportsman.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALevel_Practise7
+{
+    class VisitorsKeeper
+    {
+        public static List<Sportsman> Visitor = new List<Sportsman>();
+        public static void Add(Sportsman visitors)
+        {
+            Visitor.Add(visitors);
+        }
+        public static void Remove(Sportsman visitor)
+        {
+            Visitor.Remove(visitor);
+        }
+    }
+
+    class Sportsman
+    {
+        public string name { get; set; }
+        public int amountOfExcersices { get; set; }
+        public string reaction { get; set; }
+        public int exercisesLeft { get; set; }
+
+        public Sportsman(string name, int amountOfExcersices)
+        {
+            this.name = name;
+            this.amountOfExcersices = amountOfExcersices;
+            this.exercisesLeft = exercisesLeft = amountOfExcersices;
+            this.reaction = reaction;
+
+        }
+
+        public override string ToString()
+        {
+            if (exercisesLeft > 0)
+            {
+                reaction = "Why so long?";
+            }
+            else
+            {
+                reaction = "The client finished the exersices";
+
+            }
+                return $" Sportsman: {name} \n" +
+                       $"Total amount of the exercises {amountOfExcersices}\n" +
+                       $"Exercises left {exercisesLeft} \n" +
+                       $"{reaction} \n";
+        }
+    }
+}
