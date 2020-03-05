cb4e737be79a678cb947054378470e7f05088026
Peter <petr.chernykh.97@gmail.com>
2020-03-05 13:10:41 +0200

    ChangeSpacesMethodChanged

--- Homework12_ThreeLayer_Entity/Homework12_BLL/Services/CarService.cs
+++ Homework12_ThreeLayer_Entity/Homework12_BLL/Services/CarService.cs
@@ -27,7 +27,7 @@ namespace Homework12_BLL.Services
         {
             var car = new Car
             {
-                Model = CheckNameSpaces(carModel.Model),
+                Model = carModel.Model,
                 Details = carModel.Details.Select(x => new Detail
                 {
                     Name = x.Name,
@@ -121,36 +121,6 @@ namespace Homework12_BLL.Services
             };
 
             return carModel;
-        }
-        public string CheckNameSpaces(string name)
-        {
-            int spaceCount =0;
-
-            for(int i =0; i<name.Length; i++)
-            {
-               if(name[i] == ' ')
-                {
-                    spaceCount++;
-                }
-               else if(spaceCount == 2)
-                {
-                    throw new NotImplementedException();
-                }
-            }
-
-            return name;
-            
-
-
-            //    int l = strlen(str);
-            //int i, cnt = 0;
-            //for (i = 0; i < l; ++i)
-            //    if (str[i] == ' ')
-            //        cnt++;
-            //printf("Spaces: %d\n", cnt);
-            //return 0;
-
-
         }
     }
 }
